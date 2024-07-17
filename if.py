abf5f1c098c18b2083ef10f9ba435116182a1c33
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:01:47 -0300

    primeiro commit

---
+++ if.py
@@ -0,0 +1,18 @@
+# verificar se é maior de idade
+
+maiorIdade = 18
+idade = int(input('Digite sua idade\n'))
+
+
+if (idade >= maiorIdade):
+    print('eh maior de idade')
+else:
+    print('Eh menor de idade')
+
+# if (variavel "sinal" variavel)
+# é igual/diferemte?
+# Igual: ==
+# Diferente !=
+# é maior/menor?
+# Maior: > (Maior ou igual: >=)
+# Menor: < (Menor ou igual: <=)
